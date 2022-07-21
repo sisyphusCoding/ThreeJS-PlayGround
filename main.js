9d83756eda35040f8cd119a86190653ed1542261
Anish Kanna <sautronik@gmail.com>
2022-07-21 17:24:41 +0530

    updated

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-
+import gsap from 'gsap'
 
 const canvas = document.querySelector('#canvas')
+
+const mainWrap = document.querySelector('#mainWrap')
 
 
 const scene = new THREE.Scene()
@@ -33,7 +35,9 @@ light.position.set(-2,5,10)
 light.castShadow = true
 scene.add(light)
 
-const material = new THREE.MeshPhongMaterial({color:0x1F4690})
+const material = new THREE.MeshPhongMaterial({
+  color:0x1F4690,
+})
 const objectDistance = 6
 
 
@@ -62,6 +66,38 @@ mesh3.position.y = -objectDistance * 2
 
 scene.add(mesh1,mesh2,mesh3)
 
+
+const particlesCount  = 5000
+
+const positions = new Float32Array(particlesCount*3)
+
+for (let id = 0 ; id<particlesCount; id++){
+
+  positions[id * 3 + 0] =  (Math.random() - 0.5) * 10
+
+  positions[id * 3 + 1] = objectDistance * 0.5 - Math.random() * objectDistance *3
+
+  positions[id * 3 + 2] =  (Math.random() - 0.5) * 10
+
+}
+
+const particlesGeometry = new THREE.BufferGeometry()
+
+particlesGeometry.setAttribute('position',
+new THREE.BufferAttribute(
+positions,3
+))
+
+const particlesMaterial = new THREE.PointsMaterial({
+    color:0xffffff,
+    size:0.02,sizeAttenuation:true
+})
+
+
+const particles = new THREE.Points(particlesGeometry,particlesMaterial)
+
+scene.add(particles)
+
 const sectionMesh = [mesh1,mesh2,mesh3]
 const group = new THREE.Group()
 
@@ -74,8 +110,6 @@ const camera  = new THREE.PerspectiveCamera(
   100)
 camera.position.set(0,0,6)
 group.add(camera)
-
-
 
 const renderer = new THREE.WebGLRenderer({
   canvas:canvas,
@@ -90,7 +124,8 @@ renderer.setPixelRatio(Math.min(
   2
 ))
 
-let scrollY = window.scrollY
+let scrollY = mainWrap.scrollTop
+let currentSection = 0
 const cursor ={}
 cursor.x = 0
 cursor.y =0
@@ -100,28 +135,53 @@ window.addEventListener('mousemove',(_e)=>{
 cursor.x = _e.clientX / sizes.w - 0.5
 cursor.y = _e.clientY / sizes.h - 0.5
 
-  console.log(cursor)
 })
 
 
-window.addEventListener('scroll',()=>{
-  scrollY = window.scrollY
+mainWrap.addEventListener('scroll',()=>{
+
+  scrollY = mainWrap.scrollTop
+
+  const newSection = Math.round(scrollY / sizes.h)
+  if(newSection !== currentSection){
+    currentSection = newSection
+    gsap.to(
+      sectionMesh[currentSection].rotation,
+      {
+        duration:1.5,
+        ease:'power2.inOut',
+        x:'+=6',
+        y:'+=3',
+        z:'+=1.5'
+      }
+    )
+
+
+  }
+
+  console.log(newSection)
 })
 const clock = new THREE.Clock()
+let previousTime = 0
 const tick  = ( ) =>{
 
-
   camera.position.y = - scrollY/sizes.h * objectDistance
+
 
   const parallax ={x:cursor.x , y:-cursor.y}
   const elTime = clock.getElapsedTime()
 
+  const deltaTime = elTime - previousTime
+  
+  previousTime = elTime
+
   sectionMesh.forEach(thisMesh => {
-    thisMesh.rotation.x = elTime * .1
-    thisMesh.rotation.y = elTime * .15
+    thisMesh.rotation.x += deltaTime * .3
+    thisMesh.rotation.y += deltaTime * .3
   })
-  group.position.x += (parallax.x - group.position.x) * 0.08
-  group.position.y += (parallax.y  - group.position.y) * 0.08
+
+  group.position.x += (parallax.x - group.position.x) * 8 * deltaTime
+  group.position.y += (parallax.y  - group.position.y) * 8 * deltaTime
 
   renderer.render(scene,camera)
   window.requestAnimationFrame(tick)
